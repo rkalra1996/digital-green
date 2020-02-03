a6485b5118d758e5dc2df873aeb3d681097330a1
rishabh kalra <rishabh.kalra@niit.com>
2020-02-04 00:37:13 +0530

    disabled default logging

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
   await app.listen(3000);
   console.log('server up and running at port -> ', 3000);
 }
