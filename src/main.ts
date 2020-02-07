066a58f49c72347f714916753b24585d1529eff6
rishabh kalra <rishabh.kalra@niit.com>
2020-02-07 11:00:41 +0530

    CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug'],
   });
+  // enable cors
+  app.enableCors();
   await app.listen(3000);
   console.log('server up and running at port -> ', 3000);
 }
