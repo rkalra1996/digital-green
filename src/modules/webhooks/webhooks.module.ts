4455bde424ff07c2f70f8f1eafe4befec15cd2bf
rishabh kalra <rishabh.kalra@niit.com>
2020-03-19 13:05:21 +0530

    auto delete temp store file when uploaded successfully, now working

--- src/modules/webhooks/webhooks.module.ts
+++ src/modules/webhooks/webhooks.module.ts
@@ -3,10 +3,12 @@ import { WebhookController } from './controllers/webhook/webhook.controller';
 import { GoogleCloudWebhookHandlerService } from './services/google-cloud-webhook-handler/google-cloud-webhook-handler.service';
 import { SessionsModule } from '../sessions/sessions.module';
 import {PipelineModule} from '../pipeline/pipeline.module';
+import { SharedService } from './../../services/shared/shared.service';
+import { PathResolverService } from './../../services/path-resolver/path-resolver.service';
 
 @Module({
     controllers: [WebhookController],
     imports: [SessionsModule, PipelineModule],
-    providers: [GoogleCloudWebhookHandlerService],
+    providers: [PathResolverService, SharedService, GoogleCloudWebhookHandlerService],
 })
 export class WebhooksModule {}
